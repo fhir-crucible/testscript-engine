c8780d1e5eaa2bf3ebd3ce2ad5dfdae75ad1ff5f
Jaehoon Lee <jaehoonlee@mitre.org>
2022-05-23 07:36:04 -0600

    Merge branch 'main' of https://github.com/fhir-crucible/testscript-engine

--- spec/extract_path_spec.rb
+++ spec/extract_path_spec.rb
@@ -1,14 +1,15 @@
 require 'TestScriptRunnable'
 
 describe TestScriptRunnable do
-  let(:relative_url) { ' ' }
+  let(:relative_url) { 'Patient/123' }
   let(:absolute_url) { 'https://example.com/Patient/123' }
   let(:type_coding) { FHIR::Coding.new({
     system: 'http://hl7.org/fhir/restful-interaction',
     code: 'create'
   })}
   let(:operation) { FHIR::TestScript::Setup::Action::Operation.new({
-    type: type_coding
+    type: type_coding,
+    resourceType: "Patient"
   })}
   let(:runnable) { TestScriptRunnable.new FHIR::TestScript.new(
     {
@@ -34,20 +35,39 @@ describe TestScriptRunnable do
         before { operation.url = relative_url }
 
         it 'creates a path that includes that url' do
-
+          result = runnable.extract_path operation
+          expect(result).to eq('Patient/123')
         end 
       end 
     end 
 
     context 'with targetId' do
+      before { 
+        operation.targetId = 'fixture-patient-create' 
+        operation.resource = 'Patient'
+      }
       # some sort of failure 
-    end 
+      it 'creates a path using targetId' do
+        result = runnable.extract_path operation
+      end 
+  end 
 
     context 'with params' do
-
+      before {
+        operation.params = '/${createResourceId}'
+      }
+      it 'creates a path with params' do
+        result = runnable.extract_path operation
+      end 
     end
 
     context 'with sourceId' do
+      before { 
+        operation.sourceId = 'fixture-patient-create' 
+      }
+      it 'creates a path using sourceId' do
+        result = runnable.extract_path operation
+      end 
 
     end 
   end 
