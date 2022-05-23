3b7cf186a0ded06cd5c1b8bfbc5a4ea4004d1169
Jaehoon Lee <jaehoonlee@mitre.org>
2022-05-23 10:52:50 -0600

    Added unit testing code for relative url

--- spec/extract_path_spec.rb
+++ spec/extract_path_spec.rb
@@ -1,7 +1,7 @@
 require 'TestScriptRunnable'
 
 describe TestScriptRunnable do
-  let(:relative_url) { ' ' }
+  let(:relative_url) { 'Patient/123' }
   let(:absolute_url) { 'https://example.com/Patient/123' }
   let(:type_coding) { FHIR::Coding.new({
     system: 'http://hl7.org/fhir/restful-interaction',
@@ -24,7 +24,7 @@ describe TestScriptRunnable do
       context 'absolute' do
         before { operation.url = absolute_url }
 
-        it 'creates a path that includes that url' do
+        it 'creates a path that includes absolute url' do
           result = runnable.extract_path operation
           expect(result).to eq(absolute_url)
         end 
@@ -33,22 +33,35 @@ describe TestScriptRunnable do
       context 'relative' do
         before { operation.url = relative_url }
 
-        it 'creates a path that includes that url' do
-
+        it 'creates a path that includes relative url' do
+          result = runnable.extract_path operation
+          expect(result).to eq(relative_url)
         end 
       end 
     end 
 
     context 'with targetId' do
+      before { operation.targetId = 'fixture-patient-create' }
       # some sort of failure 
-    end 
+      it 'creates a path with targetId' do
+        result = runnable.extract_path operation
+      end 
+  end 
 
     context 'with params' do
-
+      before { operation.params = '${createResourceId}' }
+      # some sort of failure 
+      it 'creates a path with targetId' do
+        result = runnable.extract_path operation
+      end 
     end
 
     context 'with sourceId' do
-
+      before { operation.sourceId = 'fixture-patient-create' }
+      # some sort of failure 
+      it 'creates a path with sourceId' do
+        result = runnable.extract_path operation
+      end 
     end 
   end 
 end 
