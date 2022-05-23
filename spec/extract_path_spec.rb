c89c8a39ba8b8ee732a6efa18442e28cb09c6eab
Jaehoon Lee <jaehoonlee@mitre.org>
2022-05-23 08:12:16 -0600

    Revert "Merge branch 'main' of https://github.com/fhir-crucible/testscript-engine"
    This reverts commit c8780d1e5eaa2bf3ebd3ce2ad5dfdae75ad1ff5f.

--- spec/extract_path_spec.rb
+++ spec/extract_path_spec.rb
@@ -1,15 +1,14 @@
 require 'TestScriptRunnable'
 
 describe TestScriptRunnable do
-  let(:relative_url) { 'Patient/123' }
+  let(:relative_url) { ' ' }
   let(:absolute_url) { 'https://example.com/Patient/123' }
   let(:type_coding) { FHIR::Coding.new({
     system: 'http://hl7.org/fhir/restful-interaction',
     code: 'create'
   })}
   let(:operation) { FHIR::TestScript::Setup::Action::Operation.new({
-    type: type_coding,
-    resourceType: "Patient"
+    type: type_coding
   })}
   let(:runnable) { TestScriptRunnable.new FHIR::TestScript.new(
     {
@@ -35,39 +34,20 @@ describe TestScriptRunnable do
         before { operation.url = relative_url }
 
         it 'creates a path that includes that url' do
-          result = runnable.extract_path operation
-          expect(result).to eq('Patient/123')
+
         end 
       end 
     end 
 
     context 'with targetId' do
-      before { 
-        operation.targetId = 'fixture-patient-create' 
-        operation.resource = 'Patient'
-      }
       # some sort of failure 
-      it 'creates a path using targetId' do
-        result = runnable.extract_path operation
-      end 
-  end 
+    end 
 
     context 'with params' do
-      before {
-        operation.params = '/${createResourceId}'
-      }
-      it 'creates a path with params' do
-        result = runnable.extract_path operation
-      end 
+
     end
 
     context 'with sourceId' do
-      before { 
-        operation.sourceId = 'fixture-patient-create' 
-      }
-      it 'creates a path using sourceId' do
-        result = runnable.extract_path operation
-      end 
 
     end 
   end 
