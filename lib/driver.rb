e61555a9aa65cc12fb079b8f83bc1adbae3e93c3
Jaehoon Lee <jaehoonlee@mitre.org>
2022-05-25 12:27:49 -0600

    Add autocreate and autodelete

--- lib/driver.rb
+++ lib/driver.rb
@@ -3,10 +3,11 @@ require_relative './TestScriptEngine'
 require './TestScriptRunnable' # TODO: Remove
 
 default_engine = TestScriptEngine.new 'http://hapi.fhir.org/baseR4'
+# 'http://test.fhir.org/r4/' 
 # Load and execute all TestScript in /TestScripts
-default_engine.load_scripts
+# default_engine.load_scripts
 # Specify a TestScript to execute
-# default_engine.load_scripts(nil, 'general_test_script.json')
+default_engine.load_scripts(nil, 'general_test_script.json')
 default_engine.make_runnables
 default_engine.execute_runnables
 default_engine.write_reports
