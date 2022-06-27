eda4b985f894d2ac4ea30aee6f324de68fb25c5a
Jason Walonoski <jwalonoski@mitre.org>
2022-06-27 10:48:35 -0400

    Optionally process command line argments.

--- lib/driver.rb
+++ lib/driver.rb
@@ -2,7 +2,22 @@ require 'pry-nav' # TODO: Remove
 require_relative './TestScriptEngine'
 require './TestScriptRunnable' # TODO: Remove
 
-default_engine = TestScriptEngine.new 'http://hapi.fhir.org/baseR4'
+test_server_url = 'http://hapi.fhir.org/baseR4'
+testscript_path = nil
+
+parameters = ARGV
+parameters.each do |parameter|
+  if parameter.start_with?('http')
+    test_server_url = parameter
+  else
+    testscript_path = parameter
+  end
+end
+
+puts "SERVER: #{test_server_url}"
+puts "TESTSCRIPTS: #{testscript_path}"
+
+default_engine = TestScriptEngine.new(test_server_url, testscript_path)
 # Load and execute all TestScript in /TestScripts
 default_engine.load_scripts
 # Specify a TestScript to execute
