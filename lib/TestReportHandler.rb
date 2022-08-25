020fe093b24ccab60ecc886d7b2945fcee313ede
Jack Fraser <37051655+Jack-Fraser16@users.noreply.github.com>
2022-08-25 10:40:18 -0700

    clean-up builder class

--- lib/TestReportHandler.rb
+++ lib/TestReportHandler.rb
@@ -33,7 +33,7 @@ module TestReportHandler
   end
 
   def fresh_testreport_builder
-    testreport_builder_template.deep_clone
+    testreport_builder_template.clone
   end
 
   def testreport_builder_template
@@ -56,6 +56,14 @@ module TestReportHandler
     testreport_builder.skip(message)
   end
 
+  def script
+    begin
+      super
+    rescue NoMethodError
+      FHIR::TestScript.new
+    end
+  end
+
 #   # <--- ---> #
 #  In the builder, the script maintained within the parent runnable is called
 #  and each of its phases -- setup, test, and teardown -- are processed in order
@@ -63,9 +71,7 @@ module TestReportHandler
 #  as any action within the script must have a corresponding action within the report.
 
   class TestReportBuilder
-    def report
-      @report ||= FHIR::TestReport.new
-    end
+    attr_accessor :fail_count, :action_count
 
     def action
       actions.first
@@ -79,65 +85,70 @@ module TestReportHandler
       actions.concat(Array(action))
     end
 
+    def report
+      @report ||= FHIR::TestReport.new
+    end
+
     def next_action
       actions.shift
       finalize_report if actions.empty?
     end
 
-    def initialize(actions_for_report = [])
-      # use_existing_actions(actions_for_report)
+    def increment_fail_count
+      self.fail_count += 1
+    end
+
+    def clone
+      builder_dup = self.deep_dup
+      builder_dup.actions.clear
+      builder_dup.instance_eval('@report = FHIR::TestReport.new(self.report.to_hash)')
+
+      clone_actions(builder_dup.report.setup, builder_dup)
+      builder_dup.report.test.each { |test| clone_actions(test, builder_dup) }
+      clone_actions(builder_dup.report.teardown, builder_dup)
+
+      builder_dup
+    end
+
+    def clone_actions(report_phase, clone)
+      report_phase.try(:action)&.each do |action|
+        clone.store_action(action.operation || action.assert)
+      end
+    end
+
+    def initialize
       report.setup = outline_setup(script.setup)
       report.test = outline_test(script.test)
       report.teardown = outline_teardown(script.teardown)
 
-      @fail_count = 0
-      @action_count = actions.length
+      self.action_count = actions.length
+      self.fail_count = 0
     end
-
-    # def user_existing_actions(actions_for_report)
-    #   return if actions_for_report.empty?
-    # end
-
-    # # def deep_clone
-    # #   builder_deep_clone = self.class.new
-    # #   builder_deep_clone.cloning_in_progress(true)
-    # #   builder_deep_clone.store_base_actions(base_actions.deep_dup)
-    # #   builder_deep_clone.build_report_outline(script)
-    # #   builder_deep_clone.cloning_in_progress(false)
-    # # end
-
-    # # def cloning_in_progress(cloning_state = nil)
-    # #   @cloning_in_progress = cloning_state if cloning_state
-    # #   @cloning_in_progress ||= false
-    # # end
-
-    # # def cloning_in_progress?
-    # #   cloning_in_progress
-    # # end
 
     def outline_setup(setup)
       return unless setup
 
-      setup_actions = setup.action.map { |action| outline_action(action) }
-
-      FHIR::TestReport::Setup.new(action: setup_actions)
+      FHIR::TestReport::Setup.new({
+        action: setup.action.map { |action| outline_action(action) }
+      })
     end
 
     def outline_test(tests)
       return if tests.empty?
 
       tests.map do |test|
-        actions = test.action.map { |action| outline_action(action) }
-        FHIR::TestReport::Test.new(action: actions)
+        FHIR::TestReport::Test.new({
+          action: test.action.map { |action| outline_action(action) }
+        })
       end
     end
 
     def outline_teardown(teardown)
       return unless teardown
 
-      teardown_actions = teardown.action.map { |action| outline_action(action) }
-
-      FHIR::TestReport::Teardown.new(action: teardown_actions)
+      FHIR::TestReport::Teardown.new({
+        action: teardown.action.map { |action| outline_action(action) }
+      })
     end
 
     def outline_action(action)
@@ -175,19 +186,19 @@ module TestReportHandler
     end
 
     def add_boilerplate
-      report.name = script.name&.gsub(/(?i)testscript/, 'TestReport')
-      report.id = script.id&.gsub(/(?i)testscript/, 'testreport')
-      report.tester = 'The MITRE Corporation'
-      report.testScript = script.url
-      report.status = 'in-progress'
       report.result = 'pending'
+      report.status = 'in-progress'
+      report.testScript = script.url
+      report.tester = 'The MITRE Corporation'
+      report.id = script.id&.gsub(/(?i)testscript/, 'testreport')
+      report.name = script.name&.gsub(/(?i)testscript/, 'TestReport')
     end
 
     def finalize_report
-      report.status = 'completed'
-      report.result = (@fail_count == 0 ? 'pass' : 'fail')
-      report.score = (@fail_count.to_f / @action_count).round(2)
       report.issued = Time.now
+      report.status = 'completed'
+      report.result = (self.fail_count.zero? ? 'pass' : 'fail')
+      report.score = (self.fail_count.to_f / self.action_count).round(2)
     end
 
     def pass
@@ -210,7 +221,7 @@ module TestReportHandler
     def fail(message = nil)
       action.result = 'fail'
       action.message = message if message
-      @fail_count += 1
+      increment_fail_count
       next_action
     end
   end
