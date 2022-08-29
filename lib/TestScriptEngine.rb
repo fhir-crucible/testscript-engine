1477a3a221ec63912f0daa54361631b62e43bc7d
Jaehoon Lee <105247995+jhlee-mitre@users.noreply.github.com>
2022-08-29 11:10:29 -0600

    Quick fix to intake TestScripts with xml format (#24)
    * Quick fix to intake TestScript with xml format
    
    * modify search for .json/.xml files
    
    * Modifed line 42 in TestScriptEngine.rb; Added two example XML TestScripts
    
    * rename for consistency
    
    Co-authored-by: Jack Fraser <37051655+Jack-Fraser16@users.noreply.github.com>

--- lib/TestScriptEngine.rb
+++ lib/TestScriptEngine.rb
@@ -11,19 +11,19 @@ class TestScriptEngine
 
   def runnables
     @runnables ||= {}
-  end 
+  end
 
   def reports
     @reports ||= {}
-  end 
+  end
 
   def root
     directory_path || "../TestScripts"
-  end 
+  end
 
   def client
-    @client ||= FHIR::Client.new(endpoint || 'localhost:3000') 
-  end 
+    @client ||= FHIR::Client.new(endpoint || 'localhost:3000')
+  end
 
   def initialize(endpoint = nil, directory_path = nil, file_name = nil)
     self.endpoint = endpoint
@@ -34,53 +34,53 @@ class TestScriptEngine
   # TODO: Tie-in stronger validation. Possibly, Inferno validator.
   def valid_testscript? script
     return (script.is_a? FHIR::TestScript) && script.valid?
-  end 
+  end
 
-  # @path [String] Optional, specifies the path to the folder containing the 
-  #                TestScript Resources to-be loaded into the engine. 
-  def load_scripts 
-    on_deck = Dir.glob "#{root}/**/*.{json, xml}"
+  # @path [String] Optional, specifies the path to the folder containing the
+  #                TestScript Resources to-be loaded into the engine.
+  def load_scripts
+    on_deck = Dir.glob (["#{root}/**/*.{json}", "#{root}/**/*.{xml}"])
     FHIR.logger.info "[.load_scripts] TestScript Path: [#{root}]"
 
     on_deck.each do |resource|
       next if resource.include? "/fixtures/"
       next if file_name && !resource.include?(file_name)
 
-      begin 
+      begin
         script = FHIR.from_contents File.read(resource)
         if valid_testscript? script
           script.url = resource
-          scripts[script.id] = script 
+          scripts[script.id] = script
           FHIR.logger.info "[.load_scripts] TestScript with id [#{script.id}] loaded."
         else
           FHIR.logger.info "[.load_scripts] Invalid or non-TestScript detected. Skipping resource at #{resource}."
           FHIR.logger.info "[.load_scripts] Violation: #{script.validate.to_hash}"
-        end 
+        end
       rescue
         FHIR.logger.info "[.load_scripts] Unable to deserialize TestScript resource at #{resource}."
-      end 
-    end 
+      end
+    end
   end
 
-  # @script [FHIR::TestScript] Optional, a singular TestScript resource to be 
-  #                            transformed into a runnable. If no resource is 
-  #                            given, all stored TestScript are by default 
-  #                            transformed into and stored as runnables. 
+  # @script [FHIR::TestScript] Optional, a singular TestScript resource to be
+  #                            transformed into a runnable. If no resource is
+  #                            given, all stored TestScript are by default
+  #                            transformed into and stored as runnables.
   def make_runnables script = nil
     begin
       if valid_testscript? script
         runnables[script.id] = TestScriptRunnable.new script
         FHIR.logger.info "[.make_runnables] Generated runnable from TestScript with id [#{script.id}]."
-      else 
+      else
         scripts.each do |_, script|
           runnables[script.id] = TestScriptRunnable.new script
           FHIR.logger.info "[.make_runnables] Generated runnable from TestScript with id [#{script.id}]."
-        end 
-      end 
+        end
+      end
     rescue => e
       FHIR.logger.error "[.make_runnables] Unable to generate runnable. Caught error: #{e.message}."
-    end 
-  end 
+    end
+  end
 
   # TODO: Clean-up, possibly modularize into a pretty_print type method
   # @runnable_id [String] Optional, specifies the id of the runnable to be
@@ -89,30 +89,30 @@ class TestScriptEngine
     if runnable_id
       if runnables[runnable_id]
         puts "\nBeggining execution of #{runnable_id}.\n\n"
-        reports[runnable_id] = runnable.execute client 
+        reports[runnable_id] = runnable.execute client
         puts "\nFinished execution of #{runnable_id}. Score: #{reports[runnable_id].score} \n"
       else
-         FHIR.logger.info "[.execute_runnables] No runnable stored with id [#{runnable_id}]." 
-      end 
+         FHIR.logger.info "[.execute_runnables] No runnable stored with id [#{runnable_id}]."
+      end
     else
       runnables.each do |id, runnable|
         puts "\nBeggining execution of #{id}.\n\n"
         reports[id] = runnable.run client
         puts "\nFinished execution of #{id}. Score: #{reports[id].score} \n"
-      end 
+      end
     end
-  end 
+  end
 
-  # @path [String] Optional, specifies the path to the folder which the 
+  # @path [String] Optional, specifies the path to the folder which the
   #                TestReport resources should be written to.
   def write_reports path = nil
     report_directory = path || "#{root.split('/')[0...-1].join}/TestReports"
     FileUtils.mkdir_p report_directory
 
-    reports.each do |_, report| 
+    reports.each do |_, report|
       File.open("#{report_directory}/#{report.name.downcase.split(' ')[1...].join('_')}.json", 'w') do |f|
         f.write(report.to_json)
-      end 
-    end 
-  end 
+      end
+    end
+  end
 end
