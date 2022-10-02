f9a52a73b9977926f9cd4caaf463071989687cb6
Jack Fraser <37051655+Jack-Fraser16@users.noreply.github.com>
2022-10-01 18:30:21 -0700

    update testscript gem executable w/ CLI call

--- lib/testscript_engine.rb
+++ lib/testscript_engine.rb
@@ -2,6 +2,7 @@ require 'pry-nav'
 require 'fhir_client'
 require_relative 'testscript_engine/testscript_runnable'
 require_relative 'testscript_engine/message_handler'
+require_relative 'testscript_engine/cli'
 
 class TestScriptEngine
   prepend MessageHandler
