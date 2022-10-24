60f25e2dce7473d3171661037155588a313786ce
Jack Fraser <37051655+Jack-Fraser16@users.noreply.github.com>
2022-10-24 09:47:18 -0700

    update validation path in unit tests

--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative '../lib/testscript_engine/validation'
+require_relative '../lib/testscript_engine/validation/validation'
 require_relative '../lib/testscript_engine/message_handler'
 
 class ValidationTestClass
