661b5097b27bedd849c4910994848ac34bbeda56
Jack Fraser <37051655+Jack-Fraser16@users.noreply.github.com>
2022-10-24 20:34:17 -0700

    remove run.rb script

--- run.rb
+++
@@ -1,5 +0,0 @@
-cmd = `gem list -d 'fhir_client' | grep -E 'Installed at:\s+(.*)$' | cut -d ":" -f 2 | tr -d '[:space:]'`
-$LOAD_PATH.unshift("#{cmd}/gems/fhir_client-5.0.3/lib") unless cmd.empty?
-
-require_relative 'lib/testscript_engine'
-TestScriptEngine::CLI.start
