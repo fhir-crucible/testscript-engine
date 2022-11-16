277fb609efe467ebe39df3e54a19e0b238e625f1
Jaehoon Lee <jaehoonlee@mitre.org>
2022-11-16 13:34:31 -0500

    Fix http code 200 -> 2x

--- lib/testscript_engine/testscript_runnable.rb
+++ lib/testscript_engine/testscript_runnable.rb
@@ -182,7 +182,7 @@ class TestScriptRunnable
 
       profile_server = FHIR::Client.new("")
       response = profile_server.send(:get, profile.reference, { 'Content-Type' => 'json' })
-      next if response.response[:code].starts_with?('2')
+      next if response.response[:code].to_s.starts_with?('2')
 
       profiles[profile.id] = FHIR.from_contents(response.response[:body].to_s)
       info(:loaded_profile, profile.id, profile.reference)
--- spec/load_profiles_spec.rb
+++ spec/load_profiles_spec.rb
@@ -4,26 +4,25 @@ require_relative '../lib/testscript_engine/output/message_handler'
 
 describe TestScriptRunnable do
   before(:all) do
-    # @sd = FHIR.from_contents(File.read('spec/examples/StructureDefinition-us-core-patient.json'))
     @structure_definition = File.read('spec/examples/StructureDefinition-us-core-patient.json')
+    @script = FHIR.from_contents(File.read('spec/examples/basic_testscript_profile.json'))
 
     stub_request(:get, "http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient").
-         with(
-           headers: {
+      with(
+        headers: {
        	  'Accept'=>'*/*',
        	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
        	  'Content-Type'=>'application/json',
-       	  'Host'=>'hl7.org',
-       	  'User-Agent'=>'rest-client/2.1.0 (linux x86_64) ruby/2.7.3p183'
-           }).
+       	  'Host'=>'hl7.org'
+       	  # 'User-Agent'=>'rest-client/2.1.0 (linux x86_64) ruby/2.7.3p183'
+          }).
     to_return(status: 200, body: "#{@structure_definition}", headers: {})
          
-    @script = FHIR.from_contents(File.read('spec/examples/basic_testscript_profile.json'))
-    @patient = FHIR.from_contents(File.read('spec/examples/example_patient.json'))
-    @runnable = described_class.new(@script.deep_dup, lambda { |k| {}[k] })
+    @runnable = described_class.new(@script, lambda { |k| {}[k] })
   end
 
   describe '.load_profiles' do
+    
     context "given profile with proper id and reference" do
       it "returns profile id" do
         expect(@runnable.load_profiles[0].id).to eq("patient-profile")
