ba674acc367a01d7a25e0f03383ef5f3ddf7a87b
Jaehoon Lee <jaehoonlee@mitre.org>
2022-11-16 14:57:07 -0500

    Moved minimum_id_spec file

--- spec/assertions/minimum_id_spec.rb
+++ spec/assertions/minimum_id_spec.rb
@@ -1,117 +1,42 @@
-require_relative '../../lib/testscript_engine/assertion'
-require 'fhir_models'
-require 'fhir_client'
+require_relative '../../lib/testscript_engine/minimum_id'
 
-class AssertionTestClass
-  include Assertion
-  attr_accessor :response_map, :request_map, :fixtures, :reply
+describe 'minimum_id' do
 
-  def response_map
-    @response_map
-  end
+    pt_compare = JSON.parse(File.read('./spec/fixtures/example_patient.json'))
+    pt_minimum = JSON.parse(File.read('./spec/fixtures/example_patient_min.json'))
+    pt_name_compare = JSON.parse(File.read('./spec/fixtures/patient_just_name.json'))
+    pt_name_minimum = JSON.parse(File.read('./spec/fixtures/patient_just_name_min.json'))
+    pt_twoNames_compare_jumbled = JSON.parse(File.read('./spec/fixtures/patient_two_names_jumbled.json'))
+    pt_twoNames_minimum = JSON.parse(File.read('./spec/fixtures/patient_two_names_min.json'))
+    mCODE_cs_compare = JSON.parse(File.read('./spec/fixtures/mCODE_CapabilityStatement_exampleServer.json'))
+    mCODE_cs_compare_fail = JSON.parse(File.read('./spec/fixtures/mCODE_CapabilityStatement_exampleServer_shouldFail.json'))
+    mCODE_cs_minimum = JSON.parse(File.read('./spec/fixtures/mCODE_CapabilityStatement_ConditionSearch.json'))
 
-  def request_map
-    @request_map
-  end
+    describe 'use two FHIR resources to examine minimum of the other' do
 
-  def reply
-    @reply
-  end
+        context 'with pt_minimum, pt_compare' do
+            it 'returns false' do
+                expect(exam_minimum(pt_minimum.flatten_with_path, pt_compare.flatten_with_path)).to be(false)
+            end
+        end
+    
+        context 'with pt_twoNames_minimum, pt_twoNames_compare_jumbled' do
+            it 'returns false' do
+                expect(exam_minimum(pt_twoNames_minimum.flatten_with_path, pt_twoNames_compare_jumbled.flatten_with_path)).to be(false)
+            end
+        end
 
-  def evaluate_expression(input, resource)
-    FHIRPath.evaluate(input, resource.to_hash)
-  end
-end
+        context 'with mCODE_cs_minimum, mCODE_cs_compare' do
+            it 'returns false' do
+                expect(exam_minimum(mCODE_cs_minimum.flatten_with_path, mCODE_cs_compare.flatten_with_path)).to be(false)
+            end
+        end
 
-describe Assertion do
-  before(:each) do
-    @tester = AssertionTestClass.new
-    @patient_id = 'patient_id'
-    @patient_min_id = 'patient_min_id'
-
-    patient = FHIR.from_contents(File.read('spec/examples/example_patient.json'))
-    patient_min = FHIR.from_contents(File.read('spec/fixtures/example_patient_min.json'))
-    @patient = FHIR.from_contents(File.read('spec/examples/example_patient.json'))
-    @patient_min = FHIR.from_contents(File.read('spec/fixtures/example_patient_min.json'))
-    url = 'https://example.com'
-    header = { 'Content-Type' => 'Content-Type-value' }
-    request = { method: :get, url: url, path: "Patient/123", headers: header }
-    response = { code: 200, headers: header, body: patient.to_json }
-    client = FHIR::Client.new(url)
-    @client_reply = FHIR::ClientReply.new(request, response, client)
-    @client_reply_min = FHIR::ClientReply.new(request, response, client)
-    @client_reply.resource = patient
-    @client_reply.response[:body] = patient
-    @client_reply_min.resource = patient_min
-    @client_reply_min.response[:body] = patient_min
-    @tester.reply = @client_reply.deep_dup
-    @assert = FHIR::TestScript::Setup::Action::Assert.new
-    @tester.request_map = { @patient_id => @client_reply.request }
-    @tester.response_map = { @patient_id => @client_reply.response, @patient_min_id => @client_reply_min.response }
-    @tester.fixtures = { }
-  end
-
-  describe '.deep_merge' do
-    context 'with same response and minimum fixture' do
-      it 'merged result is same with response' do
-        expect(@tester.deep_merge(@patient.to_hash, @patient.to_hash)).to eq(@patient.to_hash)
-      end
+        context 'with mCODE_cs_minimum, mCODE_cs_compare_fail' do
+            it 'returns false' do
+                expect(exam_minimum(mCODE_cs_minimum.flatten_with_path, mCODE_cs_compare_fail.flatten_with_path)).to be(false)
+            end
+        end
     end
 
-    context 'with same minimized response and minimized minimum fixture' do
-      it 'merged result is same with minimized response' do
-        expect(@tester.deep_merge(@patient_min.to_hash, @patient_min.to_hash)).to eq(@patient_min.to_hash)
-      end
-    end
-
-    context 'with response supersedes minimium fixture' do
-      it 'merged result is same with response' do
-        expect(@tester.deep_merge(@patient.to_hash, @patient_min.to_hash)).to eq(@patient.to_hash)
-      end
-    end
-
-    context 'with minimium fixture supersedes response' do
-      it 'merged result is not same with minimized response' do
-        expect(@tester.deep_merge(@patient_min.to_hash, @patient.to_hash)).not_to eq(@patient_min.to_hash)
-      end
-    end
-
-  end
-
-  describe '.minimum_id' do
-    context 'with sourceId and minimumId' do
-      before {      
-        @assert.minimumId = @patient_min_id
-        @assert.sourceId = @patient_id
-      }
-
-      it 'minimum fixture is minimum of response' do
-        expect(@tester.minimum_id(@assert)).to eq(@tester.pass_message('minimumId', @assert.sourceId, 'minimumId', @assert.minimumId))
-      end
-    end
-
-    context 'with sourceId and minimumId' do
-      before {      
-        @assert.minimumId = @patient_id
-        @assert.sourceId = @patient_min_id
-      }
-
-      it 'minimum fixture is minimum of response' do
-        expect(@tester.minimum_id(@assert)).to eq(@tester.pass_message('minimumId', @assert.sourceId, 'minimumId', @assert.minimumId))
-      end
-    end
-
-    context 'with only minimumId' do
-      before {      
-        @assert.minimumId = @patient_id
-      }
-
-      it 'minimum fixture is minimum of response' do
-        expect(@tester.minimum_id(@assert)).to eq(@tester.pass_message('minimumId', @assert.sourceId, 'minimumId', @assert.minimumId))
-      end
-    end
-
-  end
-
-  
 end
